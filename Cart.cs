509c71cfa0ef3b9178f67df7f5742bc78c66dd9b
anjumunoth <57306435+anjumunoth@users.noreply.github.com>
2021-08-12 13:06:48 +0530

    Add files via upload

---
+++ Cart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebAssemblyProject.Client.Shared
+{
+    public class Cart
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int QuantityPurchased { get; set; }
+
+        public Cart(int productId, string productName, float price, int quantity, string description, string imageUrl, int quantityPurchased)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+            Quantity = quantity;
+            Description = description;
+            ImageUrl = imageUrl;
+            QuantityPurchased = quantityPurchased;
+        }
+
+        public Cart() { }
+
+        public override string ToString()
+        {
+            string str = $"ProductId : {ProductId}; ProductName: {ProductName}; Price : {Price}; Quantity Purchased:{QuantityPurchased}; Description: {Description}";
+            return str;
+        }
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebAssemblyProject.Client.Shared
+{
+    public class Products
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public Products(int productId, string productName, float price, int quantity, string description, string imageUrl)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+            Quantity = quantity;
+            Description = description;
+            ImageUrl = imageUrl;
+        }
+
+        public override string ToString()
+        {
+            string str = $"ProductId : {ProductId}; ProductName: {ProductName}; Price : {Price}; Quantity:{Quantity}; Description: {Description}";
+            return str;
+        }
+
+        public Products()
+        {
+        }
+
+    }
+}
